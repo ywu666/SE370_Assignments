6681f42b23b1a5ebe6ae436265006fa389de5215
Yujia Wu <49899381+ywu666@users.noreply.github.com>
2020-08-21 09:39:04 +0800

    Create a1.3Temp.c

---
+++ a1.3Temp.c
@@ -0,0 +1,181 @@
+/*
+    The sorting program to use for Operating Systems Assignment 1 2020
+    written by Robert Sheehan
+
+    Modified by: Yujia Wu
+    UPI: ywu660
+
+    By submitting a program you are claiming that you and only you have made
+    adjustments and additions to this code.
+ */
+
+#include <pthread.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <stdbool.h>
+#include <sys/times.h>
+
+#define SIZE    10
+
+struct block {
+    int size;
+    int *data;
+};
+
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void print_data(struct block my_data) {
+    for (int i = 0; i < my_data.size; ++i)
+        printf("%d ", my_data.data[i]);
+    printf("\n");
+}
+
+/* Split the shared array around the pivot, return pivot index. */
+int split_on_pivot(struct block my_data) {
+    int right = my_data.size - 1;
+    int left = 0;
+    int pivot = my_data.data[right];
+    while (left < right) {
+        int value = my_data.data[right - 1];
+        if (value > pivot) {
+            my_data.data[right--] = value;
+        } else {
+            my_data.data[right - 1] = my_data.data[left];
+            my_data.data[left++] = value;
+        }
+    }
+    my_data.data[right] = pivot;
+    return right;
+}
+
+
+void quick_sort(struct block my_data);
+bool is_sorted(struct block my_data);
+void *quick_sort_thread(void *my_data) {
+    struct block *cast = (struct block*) my_data;
+    
+    pthread_mutex_lock(&mutex);
+    while(is_sorted(*cast)) {
+    	pthread_cond_wait(&cond, &mutex);
+    }
+
+    quick_sort(*cast);
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+/* Quick sort the data. */
+void quick_sort(struct block my_data) {
+    if (my_data.size < 2)
+        return;
+    int pivot_pos = split_on_pivot(my_data);
+
+    struct block left_side, right_side;
+
+    left_side.size = pivot_pos;
+    left_side.data = my_data.data;
+    right_side.size = my_data.size - pivot_pos - 1;
+    right_side.data = my_data.data + pivot_pos + 1;
+
+    quick_sort(left_side);
+    quick_sort(right_side);
+}
+
+/* Check to see if the data is sorted. */
+bool is_sorted(struct block my_data) {
+    bool sorted = true;
+    for (int i = 0; i < my_data.size - 1; i++) {
+        if (my_data.data[i] > my_data.data[i + 1])
+            sorted = false;
+    }
+    return sorted;
+}
+
+/* Fill the array with random data. */
+void produce_random_data(struct block my_data) {
+    srand(1); // the same random data seed every time
+    for (int i = 0; i < my_data.size; i++) {
+        my_data.data[i] = rand() % 1000;
+    }
+}
+
+int main(int argc, char *argv[]) {
+	long size;
+
+	if (argc < 2) {
+		size = SIZE;
+	} else {
+		size = atol(argv[1]);
+	}
+    struct block start_block;
+    start_block.size = size;
+    start_block.data = (int *)calloc(size, sizeof(int));
+    if (start_block.data == NULL) {
+        printf("Problem allocating memory.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    produce_random_data(start_block);
+
+    //if (start_block.size < 1001)
+        //print_data(start_block);
+
+    struct tms start_times, finish_times;
+    times(&start_times);
+    printf("start time in clock ticks: %ld\n", start_times.tms_utime);
+
+  
+    //Split the data by using the pivot point 
+    int pivot_pos = split_on_pivot(start_block);
+    struct block left_side, right_side;
+    left_side.size = pivot_pos;
+    left_side.data = start_block.data;
+    right_side.size = start_block.size - pivot_pos - 1;
+    right_side.data = start_block.data + pivot_pos + 1;
+
+    //Still use the main thread to quick sort the right_side on it
+    pthread_mutex_lock(&mutex);
+    
+    quick_sort(right_side);
+
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
+    
+    //Declare a thread
+    pthread_t thread2;
+
+    //Create a second thread to quick sort the left_block on it
+    if (pthread_create(&thread2, NULL, quick_sort_thread, &left_side) != 0) {
+        fprintf(stderr, "ERROR: Failed to create second thread\n");
+         exit(EXIT_FAILURE);
+     }
+
+
+    //pthread_mutex_lock(&mutex);
+   // while (!is_sorted(right_side))
+        //pthread_cond_wait(&cond, &mutex);
+
+    //Wait for the second thread to finish
+    if (pthread_join(thread2, NULL) != 0) {
+        fprintf(stderr, "ERROR: Failed to join thread\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //pthread_mutex_unlock(&mutex);
+
+
+    times(&finish_times);
+    printf("finish time in clock ticks: %ld\n", finish_times.tms_utime);
+
+    // Print the result after finished the sorting
+    //if (start_block.size < 1001)
+        //print_data(start_block);
+
+    printf(is_sorted(start_block) ? "sorted\n" : "not sorted\n");
+    free(start_block.data);
+    exit(EXIT_SUCCESS);
+}
