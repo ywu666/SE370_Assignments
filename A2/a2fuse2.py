1b2ca99eaba3f85ab669883109d047af4134b071
Yujia Wu <49899381+ywu666@users.noreply.github.com>
2020-10-06 13:10:08 +0800

    Update a2fuse2.py

--- A2/a2fuse2.py
+++ A2/a2fuse2.py
@@ -3,15 +3,14 @@
 
 from __future__ import print_function, absolute_import, division
 
-import logging
-
 import os
 import sys
 
 from collections import defaultdict
+from errno import ENOENT
 from time import time
-from stat import S_IFDIR, S_IFLNK, S_IFREG
-from fuse import FUSE, LoggingMixIn, Operations
+from stat import S_IFDIR, S_IFREG
+from fuse import FUSE, LoggingMixIn, Operations, FuseOSError
 from memory import Memory
 
 
@@ -20,34 +19,37 @@ class A2Fuse2(LoggingMixIn, Operations):
     def __init__(self, root1, root2):
         self.root1 = root1
         self.root2 = root2
-        self.memory = Memory()
-        # self.files = {}
-        # self.data = defaultdict(bytes)
-        # self.fd = 0
-        # now = time()
-        # self.files['/'] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now,
-        #                        st_mtime=now, st_atime=now, st_nlink=2)
+        self.files = {}
+        self.data = defaultdict(bytes)
+        self.fd = 0
+        now = time()
+        self.files['/'] = dict(st_mode=(S_IFDIR | 0o755), st_ctime=now,
+                               st_mtime=now, st_atime=now, st_nlink=2)
 
     # Helpers
     # =======
     def _full_path(self, partial):
-        paths = []
         if partial.startswith("/"):
             partial = partial[1:]
+
+        paths = []
         path1 = os.path.join(self.root1, partial)
         path2 = os.path.join(self.root2, partial)
-
         paths.append(path1)
         paths.append(path2)
 
         return paths
 
     def create(self, path, mode, fi=None):
-        # create in memory
-        self.memory.create(path, mode)
+        self.files[path] = dict(st_mode=(S_IFREG | mode), st_nlink=1, st_uid=os.getuid(), st_gid=os.getuid(),
+                                st_size=0, st_ctime=time(), st_mtime=time(),
+                                st_atime=time())
+
+        self.fd += 1
+        return self.fd
 
     def getattr(self, path, fh=None):
-        if path not in self.memory.files:
+        if path not in self.files:
             full_paths = self._full_path(path)
             for full_path in full_paths:
                 if os.path.isfile(full_path):
@@ -56,53 +58,71 @@ class A2Fuse2(LoggingMixIn, Operations):
                                                                     'st_gid', 'st_mode', 'st_mtime', 'st_nlink',
                                                                     'st_size', 'st_uid'))
         else:
-            return self.memory.getattr(path, fh)
+            if path not in self.files:
+                raise FuseOSError(ENOENT)
+            return self.files[path]
 
     def open(self, path, flags):
-        if path not in self.memory.files:
-            full_paths = self._full_path(path)
-            for full_path in full_paths:
-                return os.open(full_path, flags)
+        if path not in self.files:
+            full_path = self._full_path(path)
+            for x in full_path:
+                return os.open(x, flags)
         else:
-            self.memory.open(path, flags)
+            self.fd += 1
+            return self.fd
 
     def read(self, path, length, offset, fh):
-        if path not in self.memory.files:
+        if path not in self.files:
             os.lseek(fh, offset, os.SEEK_SET)
             return os.read(fh, length)
         else:
-            return self.memory.read(path, length, offset, fh)
+            return self.data[path][offset:offset + length]
 
     def readdir(self, path, fh):
         full_paths = self._full_path(path)
         dirents = ['.', '..']
         for full_path in full_paths:
-            if os.path.isfile(full_path):
+            if os.path.isdir(full_path):
                 dirents.extend(os.listdir(full_path))
         dirents.extend([x[1:] for x in self.files if x != '/'])
         for r in dirents:
             yield r
 
     def unlink(self, path):
-        if path not in self.memory.files:
+        if path not in self.files:
             full_paths = self._full_path(path)
             for full_path in full_paths:
                 return os.unlink(full_path)
         else:
-            self.memory.unlink(path)
+            self.files.pop(path)
 
     def write(self, path, buf, offset, fh):
-        if path not in self.memory.files:
+        if path not in self.files:
             os.lseek(fh, offset, os.SEEK_SET)
             return os.write(fh, buf)
         else:
-            self.memory.write(path, buf, offset, fh)
+            self.data[path] = self.data[path][:offset] + buf
+            self.files[path]['st_size'] = len(self.data[path])
+            return len(buf)
 
     # File methods
     # ============
+
     def flush(self, path, fh):
         if path not in self.files:
             return os.fsync(fh)
+
+    # def release(self, path, fh):
+    #     if path not in self.files:
+    #         return os.close(fh)
+    #     else:
+    #         return 0
+    #
+    # def fsync(self, path, fdatasync, fh):
+    #     if path not in self.files:
+    #         return self.flush(path, fh)
+    #     else:
+    #         return 0
 
 
 # override
